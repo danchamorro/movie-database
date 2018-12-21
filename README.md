2b2dd920ac2f2adf2a65c7de5585466608076a72
danchamorro <dan@danchamorro.com>
2018-12-21 18:40:49 -0500

    Added DefaultProps to Movie component

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a movie database app. This app will pull in new movie listings from an API.
 
 # Includes
+
+- Prop Types
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Movie extends Component {
   render() {
-    return <div>{this.props.movie.title}</div>;
+    return (
+      <div>
+        <h3>{this.props.movie.title}</h3>
+        <p>{this.props.movie.desc}</p>
+      </div>
+    );
   }
 }
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string
+};
+
+Movie.defaultProps = {
+  desc: "No description available"
+};
 
 export default Movie;
