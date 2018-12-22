618b17ee2f67d3b535059e0fe589bd2b1dc22da9
danchamorro <dan@danchamorro.com>
2018-12-22 00:06:15 -0500

    Added ENV

--- src/MovieList.js
+++ src/MovieList.js
@@ -11,7 +11,9 @@ class MovieList extends Component {
   componentDidMount() {
     axios
       .get(
-        "https://api.themoviedb.org/3/discover/movie?api_key=1c10ecb6e88ca14221c98ce286aa3fab&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+        `https://api.themoviedb.org/3/discover/movie?api_key=${
+          process.env.REACT_APP_TMDB_KEY
+        }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
       )
       .then(res => {
         this.setState({
