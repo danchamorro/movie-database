211995585f907b81f47b93d57c52aa5614f53a02
danchamorro <dan@danchamorro.com>
2018-12-21 20:50:46 -0500

    Refactored Movie component to functional stateless component

--- src/Movie.js
+++ src/Movie.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class Movie extends Component {
-  static propTypes = {
-    movie: PropTypes.shape({
-      title: PropTypes.string.isRequired
-    })
-  };
-
-  render() {
-    return (
-      <div>
-        <h3>{this.props.movie.title}</h3>
-      </div>
-    );
-  }
-}
+const Movie = ({ movie }) => {
+  return (
+    <div>
+      <h3>{movie.title}</h3>
+    </div>
+  );
+};
 
 export default Movie;
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired
+  })
+};
